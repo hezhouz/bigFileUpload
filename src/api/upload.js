ee40d53817b70dc11a245dba1112350585287d5b
hezhouz <46963773+hezhouz@users.noreply.github.com>
2022-08-29 17:56:08 +0800

    feat: 服务端上传服务

--- src/api/upload.js
+++ src/api/upload.js
@@ -1,42 +1,43 @@
-// export function upLoadFile({
-//   url= 'http://localhost:3000 ',
-//   method = "post",
-//   data,
-//   headers = {},
-//   requestList
-// }) {
-//   return new Promise(resolve => {
-//     const xhr = new XMLHttpRequest();
-//     xhr.open(method, url);
-//     Object.keys(headers).forEach(key =>
-//       xhr.setRequestHeader(key, headers[key])
-//     );
-//     xhr.send(data);
-//     xhr.onload = e => {
-//       resolve({
-//         data: e.target.response
-//       });
-//     };
-//   });
+export function upLoadFile({
+  url= 'http://localhost:3000 ',
+  method = "post",
+  data,
+  headers = {},
+  requestList
+}) {
+  return new Promise(resolve => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    Object.keys(headers).forEach(key =>
+      xhr.setRequestHeader(key, headers[key])
+    );
+    xhr.send(data);
+    xhr.onload = e => {
+      resolve({
+        data: e.target.response
+      });
+    };
+  });
+}
+// import request from '@/utils/request'
+// const url = 'http://localhost:3000'
+
+// export function upLoadFile(params) {
+//   return request({
+//     url: url + '/uploadFile',
+//     method: 'post',
+//     params
+//   })
 // }
-import request from '@/utils/request'
 
-export function upLoadFile(params) {
-  return request({
-    url: 'uploadFile',
-    method: 'post',
-    params
-  })
-}
-
-export function merge(filename) {
-  return request({
-    url: 'merge',
-    method: 'post',
-    headers: {
-      "content-type": "application/json"
-    },
-    data: JSON.stringify(filename)
-  })
-}
+// export function merge(filename) {
+//   return request({
+//     url:  url + '/merge',
+//     method: 'post',
+//     headers: {
+//       "content-type": "application/json"
+//     },
+//     data: JSON.stringify(filename)
+//   })
+// }
 
